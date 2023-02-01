88c719fdc87c4498865972a4e84c1b0d5b9887dd
veera336 <113327537+veera336@users.noreply.github.com>
2023-02-01 14:00:48 +0530

    Add files via upload

---
+++ fibonacci with recursion.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int fib(int n)
+{
+	if(n==1)
+	return 0;
+	else if (n ==2)
+	return 1;
+	else
+	return (fib(n-1)+fib(n-2));
+}
+int main()
+{
+	int n=5;
+	int i;
+	printf("the fibonacci series is:\n");
+	for (i=1;i<=n;i++)
+	{
+		printf("%d",fib(i));
+	}
+}
+
